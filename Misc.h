ff1ad8d6ecbcf30ba6108e05b1acd32fae89addc
nsv <front_man@mail.ru>
2018-11-30 18:51:16 +0300

    added misc

--- Misc.h
+++ Misc.h
@@ -5,4 +5,20 @@
 #ifndef MARKOVS_TEXT_CHAIN_MISC_H
 #define MARKOVS_TEXT_CHAIN_MISC_H
 
+#include <array>
+#include <algorithm>
+
+constexpr auto UPPER_LET = {'A', 'B', 'C', 'D'};
+
+constexpr auto LOWER_LET = {'a', 'b', 'c', 'd'};
+
+void toLower(std::string & str) {
+  for (auto & s : str) {
+    std::find(UPPER_LET.begin(), LOWER_LET.end(), s);
+//    if (s == UPPER_LET) {
+//
+//    }
+  }
+}
+
 #endif //MARKOVS_TEXT_CHAIN_MISC_H
