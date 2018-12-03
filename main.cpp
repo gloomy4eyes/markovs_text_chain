29ff700f3ed1ba94f9a7ed5608da5c05020ff407
nsv <front_man@mail.ru>
2018-12-03 11:15:04 +0300

    added generator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "FileDownloader.h"
+#include "MarkovsChain.h"
 
 int main(int argc, char *argv[]) {
 // vpn:dfererw63dfa32@
@@ -10,8 +11,14 @@ int main(int argc, char *argv[]) {
   urls.emplace_back("file:///mnt/d/workspace/cpp/markovs_text_chain/test_urls/tu1.txt");
   urls.emplace_back("file:///mnt/d/workspace/cpp/markovs_text_chain/test_urls/tu2.txt");
   urls.emplace_back("file:///mnt/d/workspace/cpp/markovs_text_chain/test_urls/tu3.txt");
+  urls.emplace_back("file:///mnt/d/workspace/cpp/markovs_text_chain/test_urls/tu4.txt");
 //  std::list<std::string> urls{"file:///mnt/d/workspace/cpp/markovs_text_chain/test_urls/tutest.txt"};
-  FileDownloader fd(urls);
+
+  MarkovsChain mc;
+  FileDownloader fd(urls, mc);
   fd.download("urls");
+//  mc.toFile("../mc.txt");
+  mc.generate("что", 5);
+
   return 0;
 }
