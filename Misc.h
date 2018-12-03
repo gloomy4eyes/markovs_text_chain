eafba3e4e42d789f2c456ebe43f50811bbe0aa1e
nsv <front_man@mail.ru>
2018-12-03 11:15:36 +0300

    refactoring

--- Misc.h
+++ Misc.h
@@ -8,17 +8,33 @@
 #include <array>
 #include <algorithm>
 
-constexpr auto UPPER_LET = {'A', 'B', 'C', 'D'};
-
-constexpr auto LOWER_LET = {'a', 'b', 'c', 'd'};
-
-void toLower(std::string & str) {
-  for (auto & s : str) {
-    std::find(UPPER_LET.begin(), LOWER_LET.end(), s);
-//    if (s == UPPER_LET) {
+//std::array<char, 56> UPPER_LET {'A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+//                            'А', 'Б' , 'В', 'Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я'};
 //
+//std::array<char, 56> LOWER_LET {'a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+//                            'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
+//
+//
+//void toLower(std::string & str) {
+//  for (auto & s : str) {
+//    for (int i = 0; i < UPPER_LET.size(); ++i) {
+//      if (s == UPPER_LET[i]) {
+//        s = LOWER_LET[i];
+//        break;
+//      }
 //    }
-  }
-}
+//  }
+//}
+//
+//void toUpper(std::string& str) {
+//  for (auto & s : str) {
+//    for (int i = 0; i < LOWER_LET.size(); ++i) {
+//      if (s == LOWER_LET[i]) {
+//        s = UPPER_LET[i];
+//        break;
+//      }
+//    }
+//  }
+//}
 
 #endif //MARKOVS_TEXT_CHAIN_MISC_H
